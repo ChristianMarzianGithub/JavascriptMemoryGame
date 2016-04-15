b64c06091688d3cb24fa5b9798248cdf635177a6
randwerArbeit <marzian@innosoft.de>
2016-04-15 15:52:17 +0200

    asdf

---
+++ bin/main.js
@@ -0,0 +1,4 @@
+
+alert("asdf");
+document.getElementById("#td1_2").style.backgroundImage = URL("C:/Users/marzian/Pictures/MemoryGamePictures/CoverMemoryGame.bmp");
+
--- main.js
+++
@@ -1,4 +0,0 @@
-
-alert("asdf");
-document.getElementById("#td1_2").style.backgroundImage = URL("C:/Users/marzian/Pictures/MemoryGamePictures/CoverMemoryGame.bmp");
-

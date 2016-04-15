eb4eba56b9aeb956f80427bd284b609e563b81f6
randwerArbeit <marzian@innosoft.de>
2016-04-15 17:27:09 +0200

    zu 80% fetig

--- bin/main.js
+++ bin/main.js
@@ -1,4 +1,50 @@
+function cover(){
+	document.getElementById("td1").setAttribute('background', 'C:/Users/marzian/Pictures/MemoryGamePictures/CoverMemoryGame.bmp');
+	document.getElementById("td2").setAttribute('background', 'C:/Users/marzian/Pictures/MemoryGamePictures/CoverMemoryGame.bmp');
+	document.getElementById("td3").setAttribute('background', 'C:/Users/marzian/Pictures/MemoryGamePictures/CoverMemoryGame.bmp');
+	document.getElementById("td4").setAttribute('background', 'C:/Users/marzian/Pictures/MemoryGamePictures/CoverMemoryGame.bmp');
+	
+	document.getElementById("td5").setAttribute('background', 'C:/Users/marzian/Pictures/MemoryGamePictures/CoverMemoryGame.bmp');
+	document.getElementById("td6").setAttribute('background', 'C:/Users/marzian/Pictures/MemoryGamePictures/CoverMemoryGame.bmp');
+	document.getElementById("td7").setAttribute('background', 'C:/Users/marzian/Pictures/MemoryGamePictures/CoverMemoryGame.bmp');
+	document.getElementById("td8").setAttribute('background', 'C:/Users/marzian/Pictures/MemoryGamePictures/CoverMemoryGame.bmp');
+}
 
-alert("asdf");
-document.getElementById("#td1_2").style.backgroundImage = URL("C:/Users/marzian/Pictures/MemoryGamePictures/CoverMemoryGame.bmp");
+
+function shuffle(){
+	var picAddresses = [
+							"C:/Users/marzian/Pictures/MemoryGamePictures/OwlMemoryGame.bmp",
+							"C:/Users/marzian/Pictures/MemoryGamePictures/CatMemoryGame.bmp",
+							"C:/Users/marzian/Pictures/MemoryGamePictures/DuckMemoryGame.bmp",
+							"C:/Users/marzian/Pictures/MemoryGamePictures/SunMemoryGame.bmp"
+							
+	]
+	
+	var randomnumber;
+	var id = "td";
+	var tabelle = ["","","","","","","",""];
+	
+	//Ich befülle ein Array, das ich danach durchlaufe
+	// ich muss bei jeder Befüllung darauf achten, dass der zu Füllende Inhalt nicht in mehr als einem 
+	// zu füllenden Feld ist.
+	
+	
+	
+	
+	for(var i = 1; i < 9; i++){
+		randomnumber=Math.floor(Math.random()*4);	
+		id = id + i;
+		//alert(id);
+		document.getElementById(id).setAttribute('background', picAddresses[randomnumber]);	
+		id = "td";		
+	}
+	
+	
+		
+}
+
+
+
+
+
 

9e1c4d31649f8fe7b3aad98f0bb893fa0895673d
randwerArbeit <marzian@innosoft.de>
2016-04-16 22:58:01 +0200

    nichts geändert

--- bin/main.js
+++ bin/main.js
@@ -1,47 +1,60 @@
 function cover(){
-	document.getElementById("td1").setAttribute('background', 'C:/Users/marzian/Pictures/MemoryGamePictures/CoverMemoryGame.bmp');
-	document.getElementById("td2").setAttribute('background', 'C:/Users/marzian/Pictures/MemoryGamePictures/CoverMemoryGame.bmp');
-	document.getElementById("td3").setAttribute('background', 'C:/Users/marzian/Pictures/MemoryGamePictures/CoverMemoryGame.bmp');
-	document.getElementById("td4").setAttribute('background', 'C:/Users/marzian/Pictures/MemoryGamePictures/CoverMemoryGame.bmp');
+	document.getElementById("td1").setAttribute('background', '../pics/cover.png');
+	document.getElementById("td2").setAttribute('background', '../pics/cover.png');
+	document.getElementById("td3").setAttribute('background', '../pics/cover.png');
+	document.getElementById("td4").setAttribute('background', '../pics/cover.png');
 	
-	document.getElementById("td5").setAttribute('background', 'C:/Users/marzian/Pictures/MemoryGamePictures/CoverMemoryGame.bmp');
-	document.getElementById("td6").setAttribute('background', 'C:/Users/marzian/Pictures/MemoryGamePictures/CoverMemoryGame.bmp');
-	document.getElementById("td7").setAttribute('background', 'C:/Users/marzian/Pictures/MemoryGamePictures/CoverMemoryGame.bmp');
-	document.getElementById("td8").setAttribute('background', 'C:/Users/marzian/Pictures/MemoryGamePictures/CoverMemoryGame.bmp');
+	document.getElementById("td5").setAttribute('background', '../pics/cover.png');
+	document.getElementById("td6").setAttribute('background', '../pics/cover.png');
+	document.getElementById("td7").setAttribute('background', '../pics/cover.png');
+	document.getElementById("td8").setAttribute('background', '../pics/cover.png');
 }
 
 
 function shuffle(){
 	var picAddresses = [
-							"C:/Users/marzian/Pictures/MemoryGamePictures/OwlMemoryGame.bmp",
-							"C:/Users/marzian/Pictures/MemoryGamePictures/CatMemoryGame.bmp",
-							"C:/Users/marzian/Pictures/MemoryGamePictures/DuckMemoryGame.bmp",
-							"C:/Users/marzian/Pictures/MemoryGamePictures/SunMemoryGame.bmp"
+							["../pics/car.jpg",
+							"../pics/cat.jpg",
+							"../pics/house.jpg",
+							"../pics/owl.jpg"],
+							["0","0","0","0"]
 							
 	]
 	
 	var randomnumber;
 	var id = "td";
-	var tabelle = ["","","","","","","",""];
 	
 	//Ich befülle ein Array, das ich danach durchlaufe
 	// ich muss bei jeder Befüllung darauf achten, dass der zu Füllende Inhalt nicht in mehr als einem 
 	// zu füllenden Feld ist.
-	
-	
-	
+		
 	
 	for(var i = 1; i < 9; i++){
-		randomnumber=Math.floor(Math.random()*4);	
-		id = id + i;
-		//alert(id);
-		document.getElementById(id).setAttribute('background', picAddresses[randomnumber]);	
-		id = "td";		
-	}
-	
-	
+
+		randomnumber = Math.floor(Math.random()*4);	
+
+		if (picAddresses[1][randomnumber] = "0" ) {			
+			id = id + i;
+			document.getElementById(id).setAttribute('background', picAddresses[0][randomnumber]);	
+			id = "td";		
+			picAddresses[1][randomnumber] = "1";
+		};
 		
+	}		
+	
 }
+
+function nurZweiMalVorhanden(wert,arrayMitWerten){
+	var erg = false;
+
+	for (var i = 1; i < arrayMitWerten.length; i++) {
+		
+
+
+		return x;
+	};
+}
+
 
 
 
